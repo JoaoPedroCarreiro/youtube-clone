dc06cff0d4ab262a179b3a3b960fb373c8459ba5
JoaoPedroCarreiro <jpcarreirof@gmail.com>
2024-02-28 21:18:24 -0300

    top head without reponsivity

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,12 @@
 import "./App.scss"
+import "styles/low-hover.scss"
+
+import TopHead from "./top-head/TopHead"
 
 export default function App() {
     return (
         <div id="app">
-
+            <TopHead />
         </div>
     )
 }
