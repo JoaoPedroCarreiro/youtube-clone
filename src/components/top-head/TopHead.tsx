df2cbf6309036e109e52feacba7e3b75a1b631d3
JoaoPedroCarreiro <jpcarreirof@gmail.com>
2024-03-06 16:20:47 -0300

    add top head responsivity

--- src/components/top-head/TopHead.tsx
+++ src/components/top-head/TopHead.tsx
@@ -33,6 +33,10 @@ const TopHead: React.FC<TopHeadProps> = () => {
 
     const onFocusOnInput = (): void => {
         if(searchIconRightRef?.current && searchIdRef?.current) {
+            const svg = searchIconRightRef.current.querySelector("svg") as SVGSVGElement
+            svg.style.width = "20px"
+            svg.style.height = "20px"
+
             searchIconRightRef.current.style.width = "20px"
             searchIconRightRef.current.style.height = "20px"
             searchIconRightRef.current.style.marginRight = "12px"
@@ -45,6 +49,10 @@ const TopHead: React.FC<TopHeadProps> = () => {
 
     const onBlurOnInput = (): void => {
         if(searchIconRightRef?.current && searchIdRef?.current) {
+            const svg = searchIconRightRef.current.querySelector("svg") as SVGSVGElement
+            svg.style.width = "0px"
+            svg.style.height = "0px"
+
             searchIconRightRef.current.style.width = "0px"
             searchIconRightRef.current.style.height = "0px"
             searchIconRightRef.current.style.marginRight = "0px"
@@ -152,6 +160,22 @@ const TopHead: React.FC<TopHeadProps> = () => {
                 </button>
             </div>
             <div id="top-head-right">
+                <button id="search-right" className="low-hover">
+                    <span>Search</span>
+                    <div className="svg-theme-change">
+                        <svg xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" focusable="false">
+                            <path d="m20.87 20.17-5.59-5.59C16.35 13.35 17 11.75 17 10c0-3.87-3.13-7-7-7s-7 3.13-7 7 3.13 7 7 7c1.75 0 3.35-.65 4.58-1.71l5.59 5.59.7-.71zM10 16c-3.31 0-6-2.69-6-6s2.69-6 6-6 6 2.69 6 6-2.69 6-6 6z"></path>
+                        </svg>
+                    </div>
+                </button>
+                <button id="search-voice-right" className="low-hover">
+                    <span>Search with your voice</span>
+                    <div className="svg-theme-change">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false">
+                            <path d="M12 3c-1.66 0-3 1.37-3 3.07v5.86c0 1.7 1.34 3.07 3 3.07s3-1.37 3-3.07V6.07C15 4.37 13.66 3 12 3zm6.5 9h-1c0 3.03-2.47 5.5-5.5 5.5S6.5 15.03 6.5 12h-1c0 3.24 2.39 5.93 5.5 6.41V21h2v-2.59c3.11-.48 5.5-3.17 5.5-6.41z"></path>
+                        </svg>
+                    </div>
+                </button>
                 <button id="upload" className="low-hover">
                     <span>Upload</span>
                     <div className="svg-theme-change">
